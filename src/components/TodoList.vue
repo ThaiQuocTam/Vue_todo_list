f35dafaa0a78c2748ce20eb0bee0ba0f70f84e80
ThaiQuocTam <thaiquoctam123@gmail.com>
2023-07-15 12:48:01 +0700

    commit initial project

---
+++ src/components/TodoList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="container">
+        <div class="box">
+<div class="heading">
+
+</div>
+        </div>
+    </div>
+  </template>
+  
+<script>
+
+export default {
+    name: 'TodoList',
+}
+</script>
+  
+<style></style>
